ddd8839ca18d758261331fd5dd886db39ae380d0
爱华 <1348844909@qq.com>
2025-05-31 15:31:46 +0800

    testbuild

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
-// import { dirname, resolve } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import eslintPlugin from 'vite-plugin-eslint';
+console.log('CUSTOM_NODE_ENV', 'demo' === process.env.CUSTOM_NODE_ENV);
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(process.env.NODE_ENV); // "123"
+const __dirname = dirname(fileURLToPath(import.meta.url));
+let libConfig = {
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'index',
+      // 将添加适当的扩展名后缀
+      fileName: 'index'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些
+      // 你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖
+        // 提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+};
+if (process.env.CUSTOM_NODE_ENV === 'demo') {
+  libConfig = {
+    build: {}
+  };
+}
 export default defineConfig({
   base: '/vite-js-vue2-tpl',
   plugins: [
@@ -17,24 +44,7 @@ export default defineConfig({
     })
   ],
   build: {
-    // lib: {
-    //   entry: resolve(__dirname, 'src/index.js'),
-    //   name: 'index',
-    //   // 将添加适当的扩展名后缀
-    //   fileName: 'index'
-    // },
-    rollupOptions: {
-      // 确保外部化处理那些
-      // 你不想打包进库的依赖
-      // external: ['vue'],
-      // output: {
-      //   // 在 UMD 构建模式下为这些外部化的依赖
-      //   // 提供一个全局变量
-      //   globals: {
-      //     vue: 'Vue'
-      //   }
-      // }
-    }
+    ...libConfig.build
   },
   resolve: {
     alias: {
